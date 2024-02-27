41ecb4c6ca03e88eb798c1cebc8e51bbc561b1ca
aki <akshitpjt07@gmail.com>
2024-02-27 11:27:28 +0530

    render list

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import HigherOrderComponent from './component/HigherOrderComponent';
+import React from 'react';
+function App() {
+  return (
+    <>
+  <HigherOrderComponent/>
+    </>
+  );
+}
+
+export default App;
